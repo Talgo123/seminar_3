6f93817e4d0e7ded61a4a54e54e9df80d76c80a1
talgat <15talgat@mail.ru>
2022-11-30 18:53:58 +0300

    done zadacha_2

---
+++ zadacha_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.Clear();
+
+int DataNumber(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+
+void FindCoordinates(int number)
+{
+    if (number == 1) Console.WriteLine("x и y положительные");
+    if (number == 2) Console.WriteLine("x отрицательный, а y положительный");
+    if (number == 3) Console.WriteLine("x и y отрицательные");
+    if (number == 4) Console.WriteLine("x положительный, а y отрицательный");
+}
+
+int num = DataNumber("Введите номер части n = ");
+if (num> 5 || num < 0) Console.WriteLine("Номер числа от 1 до 4!");
+FindCoordinates(num);
