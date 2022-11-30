48f8f2c9799be9c2ab6ec52a0aa43489b863279b
talgat <15talgat@mail.ru>
2022-11-30 18:29:12 +0300

    done zadacha_1

---
+++ zadacha_1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+int DataEntryXY(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+
+
+void FindQandrat(int x, int y)
+{
+    if ((x > 0) && (y > 0)) Console.WriteLine("это первый квадрат");
+    if ((x < 0) && (y > 0)) Console.WriteLine("это второй квадрат");
+    if ((x < 0) && (y < 0)) Console.WriteLine("это третий квадрат");
+    if ((x > 0) && (y < 0)) Console.WriteLine("это четвертый квадрат");
+
+}
+
+
+
+int x = DataEntryXY("Введите координаты x = ");
+// Console.WriteLine(x);
+int y = DataEntryXY("Введите координаты y = ");
+// Console.WriteLine(y);
+FindQandrat(x, y);
