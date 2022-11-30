054a4cdaaf0204a1f06afbfefb7b61977a3cc102
talgat <15talgat@mail.ru>
2022-11-30 21:46:34 +0300

    добавили условие

--- zadacha_4/Program.cs
+++ zadacha_4/Program.cs
@@ -1,4 +1,8 @@
-﻿
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел
+// от 1 до N.
+
+// 5 -> 1, 4, 9, 16, 25.
+// // 2 -> 1,4
 
 void FindSquareNumber(int number)
 {
@@ -8,9 +12,7 @@ void FindSquareNumber(int number)
         int result = count * count;
         Console.Write($"{result} ");
         count++;
-    }    
-        
-    
+    }     
 }
 
 int EnterPointCoordinate(string str)
