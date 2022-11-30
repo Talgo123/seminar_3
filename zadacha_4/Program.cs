7eefbf4653364c0bfe9ac3652cda1be74986f1dd
talgat <15talgat@mail.ru>
2022-11-30 21:37:46 +0300

    done zadacha_4

---
+++ zadacha_4/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+void FindSquareNumber(int number)
+{
+    int count = 1;
+    while (count < number + 1)
+    {
+        int result = count * count;
+        Console.Write($"{result} ");
+        count++;
+    }    
+        
+    
+}
+
+int EnterPointCoordinate(string str)
+{
+    Console.Write(str);
+    int number1 = int.Parse(Console.ReadLine());
+    return number1;
+}
+
+int digit = EnterPointCoordinate("Введите цифру: ");
+FindSquareNumber(digit);
