fdcd22a82b97186a1b447efd065bf3c3401e9a86
talgat <15talgat@mail.ru>
2022-11-30 20:16:17 +0300

    исправили задачу 3

--- zadacha_3/Program.cs
+++ zadacha_3/Program.cs
@@ -1,4 +1,9 @@
-﻿
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+
 double EnterPointCoordinate(string str)
 {
     Console.WriteLine(str);
@@ -6,16 +11,17 @@ double EnterPointCoordinate(string str)
     return number1;
 }
 
-void FindDistance(double numx1, double numy2, double numx3, double numy4)
+void FindDistance(double x1, double y1, double x2, double y2)
 {
-    double distance = Math.Sqrt ((numx3 - numx1) * (numx3 - numx1) + (numy4 - numy2) * (numy4 - numy2));
-    double roundedDistance = Math.Round(distance, 2);
+    double distance = Math.Sqrt (((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+    double roundedDistance = Math.Round(distance, 2); // округлил цифры после запятой
     Console.WriteLine($"Расстояние между двумя точками в 2D пространстве = {roundedDistance}");
 }
 
 double xA = EnterPointCoordinate("Введите x координату точки А");
 double yA = EnterPointCoordinate("Введите y координату точки А");
+
 double xB = EnterPointCoordinate("Введите x координату точки В");
 double yB = EnterPointCoordinate("Введите y координату точки В");
 
-FindDistance(xA, xB, yA, yB);
+FindDistance(xA, yA, xB, yB);
